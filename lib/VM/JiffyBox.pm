c5e34819a0a51bfc8b48391e9c08b0fb1c378d23
Boris Däppen <bdaeppen.perl@gmail.com>
2013-06-03 21:18:36 +0000

    Import of BORISD/VM-JiffyBox-0.0021 from CPAN.
    gitpan-cpan-distribution: VM-JiffyBox
    gitpan-cpan-version:      0.0021
    gitpan-cpan-path:         BORISD/VM-JiffyBox-0.0021.tar.gz
    gitpan-cpan-author:       BORISD
    gitpan-cpan-maturity:     released

--- lib/VM/JiffyBox.pm
+++ lib/VM/JiffyBox.pm
@@ -1,35 +1,12 @@
 package VM::JiffyBox;
 {
-  $VM::JiffyBox::VERSION = '0.002'; # TRIAL
+  $VM::JiffyBox::VERSION = '0.0021';
 }
 
 # The line below is recognised by Dist::Zilla and taken for CPAN packaging
 # ABSTRACT: OO-API for JiffyBox Virtual Machine
 
 use Moo;
-
-use VM::JiffyBox::Box;
-
-has token => (is => 'ro', required => 1);
-has version => (is => 'ro', default => 'v1.0');
-
-sub get_id_from_name {
-    my $self = shift;
-}
-
-sub get_vm {
-    my $self = shift;
-    my $box_id = shift;
-
-    my $box = VM::JiffyBox::Box->new(id => $box_id);
-    $box->hypervisor($self);
-
-    return $box;
-}
-
-sub create_vm {
-    my $self = shift;
-}
 
 1;
 
@@ -43,44 +20,18 @@ VM::JiffyBox - OO-API for JiffyBox Virtual Machine
 
 =head1 VERSION
 
-version 0.002
-
-=head1 SYNOPSIS
-
- use VM::JiffyBox;
-
- my $jiffy = VM::JiffyBox->new($token);
-
- my $box_id = $jiffy->get_id_from_name($box_name);
- my $box    = $jiffy->get_vm($box_id);
-
- my $backup_id = $box->get_backup_id();
- my $new_box   = $jiffy->create_vm($backup_id);
- $new_box->start();
-
- my $new_box_details = $new_box->get_details();
-
- $new_box->stop();
- $new_box->delete();
-
- 1;
+version 0.0021
 
 =encoding utf8
 
-=head1 PLEASE NOTE
+=head1 ON THIS MODULE
 
-This module ist still under heavy development and a B<TRIAL> version.
-We do not recommend to use or even test it.
+This module ist still under heavy development.
+Releases including code can only be found as B<TRIAL>-uploads which are not indexed on CPAN.
 
-=head1 METHODS
+B<THIS MODULE IS A PLACEHOLDER AND CONTAINS ONLY THIS TEXT AND NO CODE>.
 
-=head2 get_id_from_name($box_name)
-
-=head2 get_vm($box_id)
-
-=head2 create_vm($backup_id)
-
-=head1 SEE ALSO
+If you wish to watch the development, please point your browser to here:
 
 =over
 
@@ -88,9 +39,11 @@ We do not recommend to use or even test it.
 
 =back
 
+Modul will be out somwhere in summer 2013.
+
 =head1 AUTHOR
 
-Tim Schwarz <todo@todo.de>, Boris Däppen <boris_daeppen@bluewin.ch>
+Tim Schwarz, Boris Däppen <boris_daeppen@bluewin.ch>
 
 =head1 COPYRIGHT AND LICENSE
 
--- t/00_load.t
+++
@@ -1,4 +0,0 @@
-use Test::More tests => 2;
-
-use_ok ('VM::JiffyBox');
-use_ok ('VM::JiffyBox::Box');
